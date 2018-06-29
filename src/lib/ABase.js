8a6e03bc102aea9e2d5f2daf2a70d91195343d45
unknown <uptimevic@gmail.com>
2018-06-29 13:24:46 -0400

    router

--- src/lib/ABase.js
+++ src/lib/ABase.js
@@ -13,7 +13,7 @@ const chokidar = require('chokidar');
 const reload = require('reload');
 class MetaAdmin {
     ver() {
-        return "v3.06.20";
+        return "v3.06.29";
     }
 }
 exports.MetaAdmin = MetaAdmin;
