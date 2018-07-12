79df70ef633cd5a1414a1174a5cb1e9774aaf157
unknown <uptimevic@gmail.com>
2018-07-12 13:37:29 -0400

    logs js

--- admin/admin_www/screens/logs/tags/first-tag.js
+++
@@ -1,7 +0,0 @@
-
-riot.tag2('first-tag', '<p>oh hi</p> <p>{num}</p>', '', '', function(opts) {
-    this.doSomething = function(arg) {
-    	console.log('XXX ', arg)
-    	this.update({num: arg})
-    }.bind(this)
-});
---
+++ admin/admin_www/screens/logs/tags/last-tag.js
@@ -0,0 +1,17 @@
+
+riot.tag2('last-tag', '<p>{last}</p>', '', '', function(opts) {
+    let thiz = this
+    this.showLast = function() {
+       console.log('show ')
+
+       window.aSrv.getLast().then(function(resp) {
+          console.log( JSON.stringify(resp.data._msg ))
+          console.log( Object.keys(resp.data._msg ))
+          let msg =  resp.data._code + ' : ' + resp.data._cmd
+          thiz.update({last: msg } )
+       }).catch(function (error) {
+           console.log(window.aSrv.getError(error))
+       })
+
+    }.bind(this)
+});
