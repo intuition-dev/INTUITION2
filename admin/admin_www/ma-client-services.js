487c7b04ca031b8f82459c3b87121a190936c449
Wolfgang Gehner <wgehner@gmail.com>
2018-07-18 16:06:30 -0700

    Update ma-client-services.js

--- admin/admin_www/ma-client-services.js
+++ admin/admin_www/ma-client-services.js
@@ -53,10 +53,10 @@ class AdminAuth {
 
 /**
 * Create new MetaAdminService instance.
-* It needs Axios loaded before, ex: https://unpkg.com/axios/dist/axios.min.js.
+* Needs Axios loaded before - e.g. https://unpkg.com/axios/dist/axios.min.js.
 * @returns MetaAdminService instance
-* @param baseUrl e.g.: 'http://localhost:9083'
-* @param secret e.g.: '123'
+* @param baseUrl - e.g. 'http://localhost:9083'
+* @param secret - e.g. '123'
 * @example
 *   loadjs('/ma-client-services.js','ma-client')
 *   loadjs.ready(['ma-client'], function () {
@@ -94,15 +94,15 @@ class MetaAdminService {
 
    /**
     * Get the last message from the last executed command
-    * @returns a promise, than( resp.dat )/catch{error}
+    * @returns a promise, then(resp.dat)/catch{error}
     * @param folder folder - e.g. '/'
     */
    getLast() {
       return this.service.get('/api/last')
    }
    /**
-    * Does an nbake 'bake' in that folder.
-    * @returns a promise, then( resp.dat )/catch{error}
+    * Does an nbake 'bake' in that folder
+    * @returns a promise, then(resp.dat)/catch{error}
     * @param folder folder - e.g. '/'
     */
    bake(folder) {
@@ -110,8 +110,8 @@ class MetaAdminService {
       return this.service.get('/api/bake'+dir)
    }
    /**
-    * Does an nbake -t 'tag process' in that folder.
-    * @returns a promise, then( resp.dat )/catch{error}
+    * Does an nbake -t 'tag process' in that folder
+    * @returns a promise, then(resp.dat)/catch{error}
     * @param folder folder - e.g. '/'
     */
    tag(folder) {
@@ -120,7 +120,7 @@ class MetaAdminService {
    }
    /**
     * Does an nbake -i 'itemize' from the mount
-    * @returns a promise, then( resp.dat )/catch{error}
+    * @returns a promise, then(resp.dat)/catch{error}
     * @param folder folder - e.g. '/blog'
     */
    itemize(folder) {
@@ -129,8 +129,8 @@ class MetaAdminService {
    }
    /**
     * Try to get title, image and desc of an url. Can be used for linkblog.
-    * @returns a promise, then( resp.dat )/catch{error}
-    * @param url e.g. 'https://www.usatoday.com/sports/'
+    * @returns a promise, then(resp.dat)/catch{error}
+    * @param url - e.g. 'https://www.usatoday.com/sports/'
     */
    scrape(url) {
       let arg = '?url='+btoa(url)
@@ -138,10 +138,10 @@ class MetaAdminService {
    }
    /**
     * Creates a new link blog item
-    * @returns a promise, then( resp.dat )/catch{error}
-    * @param src e.g. '/blog/one'
-    * @param dest e.g. '/blog/newOne'
-    * @param url e.g. 'https://www.usatoday.com/sports/'
+    * @returns a promise, then(resp.dat)/catch{error}
+    * @param src - e.g. '/blog/one'
+    * @param dest - e.g. '/blog/newOne'
+    * @param url - e.g. 'https://www.usatoday.com/sports/'
     */
    newLinkBlog(src, dest, url) {
       let arg = '?url='+btoa(url)
@@ -167,7 +167,7 @@ class MetaAdminService {
 function isLoggedIn(url) {
    const aa = new AdminAuth()
    return new Promise(function(resolve, reject) {
-      if( aa.exists() ) {
+      if (aa.exists()) {
          const  baseURL = url
          let maSrv = new MetaAdminService(baseURL, aa.secret)
          maSrv.getLast().then(function(resp) {
@@ -178,7 +178,7 @@ function isLoggedIn(url) {
             console.log(maSrv.getError(error))
             reject()
          })
-      } else //fi, no cookie
+      } else //if no cookie
          reject()
    }) // pro
 }//()
