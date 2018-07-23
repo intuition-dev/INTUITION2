71edbfb2ab37b21f3b141234cfb34f85dfd947a5
unknown <uptimevic@gmail.com>
2018-07-23 11:45:49 -0400

    6

--- adminEG/admin_www/admin-client.js
+++ adminEG/admin_www/admin-client.js
@@ -5,9 +5,9 @@
 // Version should sync w/ mbake version due to -a
 
 /**
- * Version v3.07.54
+ * Version v3.07.56
  */
-console.log('ma-client-services', 'v3.07.54')
+console.log('ma-client-services', 'v3.07.56')
 
 /**
 * Login and logout to Meta Admin Service
