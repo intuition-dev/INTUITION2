a83dbda0c8f9804f8fa0164c5f93ec26bdbd2b3a
wgehner <wgehner@gmail.com>
2018-10-18 20:05:44 +0000

    Latest

--- autoEG/index.ts
+++ autoEG/index.ts
@@ -13,6 +13,7 @@ const cors = require('cors')
 const yaml = require('js-yaml')
 const fs = require('fs')
 const bodyParser = require('body-parser')
+const slugify = require('slugify')
 
 import { RetMsg, MetaPro2, Watch2, AdminSrv, MDevSrv2, Scrape, FileOps, Dat, Ver } from 'mbake/lib/Base'
 
@@ -29,7 +30,7 @@ server.use(basicAuth({
 }))
 
 // routes ///////////////////////////////////////
-server.use(bodyParser())
+server.use(bodyParser({limit: '10mb', extended: true }))
 server.use(bodyParser.urlencoded({ extended: false }))
 server.use(bodyParser.json())
  
@@ -69,6 +70,18 @@ server.get('/api/tag', function (req, res) {
    let ret:RetMsg = mp.tag(dir)
    if(ret.code<0)
       res.status(500).send(ret)
+   else
+      res.json(ret)
+})//api
+server.get('/api/items', function (req, res) {
+   console.log(' items')
+   res.setHeader('Content-Type', 'application/json')
+   let qs = req.query
+   let dir = qs[MetaPro2.folderProp]
+
+   let ret:RetMsg = mp.getItems(dir)
+   if(ret.code<0)
+      res.status(500).send(ret.cmd)
    else
       res.json(ret)
 })//api
@@ -160,6 +173,106 @@ server.post('/api/newLinkBlog', function (req, res) {
    }
 })//api
 
+server.post('/api/newBlog', function (req, res) {
+   console.log(' newBlog')
+   res.setHeader('Content-Type', 'application/json')
+   let body = req.body
+   
+   let folder = body.folder   
+   let title = body.title
+   let comment = body.summary
+   let content = body.content
+   //let img_url = body.img_url
+   let f1 = body.f1
+   let f1name = body.f1name
+   logger.trace('f1name'+f1name)
+   //logger.trace(f1)
+
+   //create folder name from title
+   //prefix with yyy__mm__dd of publish date for sort order?
+   let dest = '/' + folder + '/'+slugify(title.toLowerCase())
+   
+   try {
+     
+     const p = config.mount + dest
+     logger.trace(p)
+     
+     let i = 1, p0 = p;
+     while (fs.existsSync(p0)) { //avoid duplicate foldernames by adding 2, 3
+       i++ 
+       p0 = p + i
+     }
+     if (i>1) dest = dest + i
+
+     fo.clone('/blog/template', dest)
+
+     let d = new Dat(p0)
+     let imgUrl = 
+     d.set('title', title)
+     d.set('comment', comment)
+     d.set('tags', body.tags)
+     //d.set('image', img_url)     
+     d.set('external_url', 'NA')
+     d.set('date_published', body.date_published )
+     d.set('publish', true ) //if false it's not included in items.json
+     d.write() //w2
+
+
+       if (f1)
+       {
+        var buffer = Buffer.from(f1.split(",")[1], 'base64')
+        
+        let f1path = dest + '/' + f1name
+        fo.write(f1path, buffer)
+         
+        //TODO:
+        /*var dimensions = Scrape.getBufferImageSize(buffer)
+         d.set('img_w',dimensions.width)
+         d.set('img_h',dimensions.height)
+         d.set('img_typ','TBD')
+         d.set('img_sz',100) //TBD
+         d.write() //w3
+         */
+        d.set('image', f1name) 
+        d.write() //w2
+         
+         
+        console.log('Writing image done IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII')
+       }
+      else //default image
+       {
+      /*Scrape.getImageSize(img_url).then(function(idata){
+         d.set('img_w',idata['width'])
+         d.set('img_h',idata['height'])
+         d.set('img_typ',idata['type'])
+         d.set('img_sz',idata['length'])
+         d.write() //w3
+      })*/
+       }
+
+      // write content
+      let md = dest+'/content.md'
+      logger.trace(md)
+      fo.write(md, content) //w4
+      console.log('Writing content done IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII')
+
+     let rex:RetMsg = mp.bake(dest)
+      console.log('Baking done IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII')
+     
+     let ret:RetMsg = mp.itemizeOnly(folder)
+     if(ret.code<0)
+        res.status(500).send(ret)
+     else
+        res.json(ret)
+      console.log('Itemize done IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII')
+       
+   } catch(err) {
+      console.log('// ERR //////////////////////////////////////////////////////')
+      console.log(err)
+   }
+})//api
+
+
 server.get('/api/clone', function (req, res) {
    console.log(' itemize')
    res.setHeader('Content-Type', 'application/json')
@@ -182,7 +295,7 @@ var listener = server.listen(config.services_port, function () {
    //console.log(server._router.stack )
 })
 
-let app = new MDevSrv2(config['mount'], config['mount_port'])
+let app = new MDevSrv2(config['mount'], config['mount_port'], true) //= ignore reload
 let admin = new AdminSrv(config)
 let w = new Watch2(mp, config['mount'])
 
