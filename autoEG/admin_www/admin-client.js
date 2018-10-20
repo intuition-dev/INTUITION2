6f8292a810a03e1d9e391c12adc03110444c975d
Vic <vic@eml.cc>
2018-10-20 09:39:28 -0400

    8

--- autoEG/admin_www/admin-client.js
+++ autoEG/admin_www/admin-client.js
@@ -5,9 +5,9 @@
 // Version should sync w/ mbake version due to -a
 
 /**
- * Version v4.11.7
+ * Version v4.11.8
  */
-console.log('ma-client-services', 'v4.11.7')
+console.log('ma-client-services', 'v4.11.8')
 
 /**
 * Login and logout to Meta Admin Service
