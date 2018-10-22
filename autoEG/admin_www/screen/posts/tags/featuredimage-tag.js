438ae664bc7aac90ca9dccfddeea28998ad65346
wgehner <wgehner@gmail.com>
2018-10-22 16:03:59 +0000

    Javascript cleanup

---
+++ autoEG/admin_www/screen/posts/tags/featuredimage-tag.js
@@ -0,0 +1,51 @@
+
+riot.tag2('featuredimage-tag', '<div class="card-body"> <input class="form-input" type="file" id="f1" placeholder="image" onchange="{upload}"> </div> <div class="card-image"><img class="img-responsive" id="f1img" riot-src="{src}"> <div id="f1del"><a hohref onclick="{deleteFeaturedImage}"> <svg class="feather feather-x" viewbox="0 0 24 24"> <line x1="18" y1="6" x2="6" y2="18"></line> <line x1="6" y1="6" x2="18" y2="18"></line> </svg></a></div> </div>', '', '', function(opts) {
+    console.log('featuredimage-tag')
+    thiz = this
+
+    this.render = function() {
+    	thiz.src = ''
+    	thiz.update()
+    }.bind(this)
+
+    this.upload = function(e) {
+    	let files = e.target.files;
+    	if (files && files[0]) {
+    		var reader = new FileReader();
+    		reader.readAsDataURL(files[0]);
+    		reader.onload = function(e) {
+    			thiz.src = e.target.result
+    			thiz.update()
+
+    			$('#featuredimage .card-body').hide();
+    			$('#f1del').show();
+    		}
+    	}
+    }.bind(this)
+
+    this.read = function(callback) {
+    	let f1 = $('#f1')[0].files[0];
+    	if (f1)
+    	{
+
+    		let reader = new FileReader();
+    		reader.readAsDataURL(f1);
+    		reader.onload = function(e) {
+    			let b64 = e.target.result
+    			callback(b64, f1.name)
+    		}
+    	}
+    	else
+    		callback()
+    }.bind(this)
+
+    this.deleteFeaturedImage = function() {
+    	depp.require(['style'], function(){
+    		$('#f1').val('')
+    		thiz.src = ''
+    		thiz.update()
+    		$('#f1del').hide();
+    		$('#featuredimage .card-body').show();
+    	})
+    }.bind(this)
+});
--- autoEG/admin_www/screen/posts/tags/medialist-tag.js
+++ autoEG/admin_www/screen/posts/tags/medialist-tag.js
@@ -1,9 +1,6 @@
 
-riot.tag2('medialist-tag', '<div class="card" id="media"> <div class="card-body"><virtual each="{items}"> <div class="tile"> <div class="tile-content"> <div class="flex edge"> <div class="left"> <div class="card"> <div class="card-title text-ellipsis">{filename}</div> <div class="card-subtitle"><a nohref alt="{filename}" onclick="copyToClipboard(\'{filename}\')"> <div class="chip">Copy Path</div></a></div> </div> </div> <div class="right"><a nohref alt="{filename}" onclick="deleteMedia(\'{filename}\')"> <figure class="avatar avatar-xl badge" data-badge="X"><img riot-src="{src}"></figure></a></div> </div> </div> </div></virtual> </div> </div>', '', '', function(opts) {
+riot.tag2('medialist-tag', '<div class="card" id="media"> <div class="card-body"><virtual each="{items}"> <div class="tile"> <div class="tile-content"> <div class="flex edge"> <div class="left"> <div class="card"> <div class="card-title text-ellipsis">{filename}</div> <div class="card-subtitle"><a nohref alt="{&quot;filename&quot;:&quot;/home/admin/Metabake-Admin/autoEG/admin_www/screen/posts/.&quot;}" onclick="{parent.copyToClipboard}"> <div class="chip">Copy Path</div></a></div> </div> </div> <div class="right"><a nohref alt="{&quot;filename&quot;:&quot;/home/admin/Metabake-Admin/autoEG/admin_www/screen/posts/.&quot;}" onclick="deleteMedia(\'{filename}\')"> <figure class="avatar avatar-xl badge" data-badge="X"><img riot-src="{src}"></figure></a></div> </div> </div> </div></virtual> </div> </div> <textarea id="copy" readonly></textarea>', 'medialist-tag #copy,[data-is="medialist-tag"] #copy{ position: absolute; left: -9999px; }', '', function(opts) {
     console.log('medialist-tag')
-    this.on('*', function(evt) {
-    	console.log('riot', evt)
-    })
     this.items = []
     thiz = this
 
@@ -17,6 +14,48 @@ riot.tag2('medialist-tag', '<div class="card" id="media"> <div class="card-body"
     		return $.extend(true, {}, obj);
     	});
     	thiz.items = cloned
+    	thiz.update()
+    }.bind(this)
+
+    this.copyToClipboard = function(e) {
+    	let str = e.item.filename
+    	$('#copy').val(str)
+    	$('#copy').select()
+    	document.execCommand('copy');
+    }.bind(this)
+
+    this.upload = function(files) {
+
+    	if (files){
+    		let len = files.length
+    		for (var i = 0; i < len; i++)
+    		{
+    			let f = files[i]
+    			if(!f.type.match('image.*')) continue
+    			let fname = f.name
+    			let dup = false, sz = thiz.items.length
+    			for(j = 0; j < sz; j++) {
+    				if(thiz.items[j].filename === fname) {dup = true; break}
+    			}
+    			if (dup) continue;
+    			let reader = new FileReader();
+    			reader.readAsDataURL(f);
+    			reader.onload = function(e) {
+    				thiz.items.push({filename: fname, src: e.target.result})
+    				thiz.update()
+    			}
+    		}
+    	}
+    }.bind(this)
+
+    this.deleteItem = function(filename) {
+    	let sz = thiz.items.length
+    	let clone = []
+    	for(i = 0; i < sz; i++) {
+    		var item = thiz.items[i]
+    		if (item.filename != filename) clone.push(item)
+    	}
+    	thiz.items = clone
     	thiz.update()
     }.bind(this)
 });
