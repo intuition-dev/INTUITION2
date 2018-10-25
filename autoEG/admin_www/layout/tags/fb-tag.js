063f527fb487b4f0706b072c9aba5f12eb11f079
wgehner <wgehner@gmail.com>
2018-10-25 00:03:46 +0000

    Login, Invite

--- autoEG/admin_www/layout/tags/fb-tag.js
+++ autoEG/admin_www/layout/tags/fb-tag.js
@@ -2,39 +2,38 @@
 riot.tag2('fb-tag', '', '', '', function(opts) {
 
     const fconfig = {
-       apiKey: 'AIzaSyCU3yQSeCsQ'+'-'+'uIDAl3ooT8LqaVWQAutHMI',
-       authDomain: 'mymeta-host.firebaseapp.com',
-       projectId: 'mymeta-host'
+    	apiKey: 'AIzaSyCU3yQSeCsQ'+'-'+'uIDAl3ooT8LqaVWQAutHMI',
+    	authDomain: 'mymeta-host.firebaseapp.com',
+    	projectId: 'mymeta-host'
     }
 
     window.firebase.initializeApp(fconfig)
-    window.auth =firebase.auth()
+    window.auth = firebase.auth()
     auth.setPersistence(firebase.auth.Auth.Persistence.LOCAL)
 
-    window.db1  = window.firebase.firestore()
-    const dsettings = {  timestampsInSnapshots: true}
-    db1.settings(dsettings)
-
     function isUserIn() {
-       if(!auth.currentUser) return false
-       return auth.currentUser.emailVerified
+    	if(!auth.currentUser) return false
+    	return auth.currentUser.emailVerified
     }
 
     auth.onAuthStateChanged(function(user_) {
-       if (isUserIn()) {
-          console.log('CRUDauth', true)
-       }
-       else {
-          console.log('CRUDauth','bye')
-       }
+    	if (isUserIn()) {
+    		console.log('CRUDauth', true)
+    	}
+    	else {
+    		console.log('CRUDauth','bye')
+    	}
     })
 
-    function sendEmailVerification() {
-       if(!isUserIn()) {
-          console.log('sending', auth.currentUser)
-          auth.currentUser.sendEmailVerification()
-       }
-       else
-          console.log('no currentUser')
-    }
+    this.sendEmailVerification = function() {
+    	if(!isUserIn()) {
+    		alert('sending', auth.currentUser)
+    		console.log('sending', auth.currentUser)
+    		auth.currentUser.sendEmailVerification()
+    	}
+    	else {
+    		alert('no currentUser')
+    		console.log('no currentUser')
+    	}
+    }.bind(this)
 });
