d978e28dab9d5c5b4d37f2f2fa34c254c04e9b24
Vic <vic@eml.cc>
2018-11-03 17:41:19 -0400

    sync

--- autoAdminEG/www/AdminService.js
+++ autoAdminEG/www/AdminService.js
@@ -1,13 +1,12 @@
 
 // npm -g i documentation
 // $ documentation build --config documentation.yml AdminService.js -f html -o api
-// Note: don't upload CSS, else fix in S3
 // Version should sync w/ mbake version 
 
 /**
  * Version v4.11.23
  */
-console.log('EditorService', 'v4.11.23')
+console.log('AdminService', 'v4.11.23')
 
 /**
 * @example
