eda6a5acfd0f34f349a02b9274ee93dfadbdd4a7
Vic <vic@eml.cc>
2018-11-03 11:30:53 -0400

    init project

---
+++ autoAdminEG/www/AdminService.js
@@ -0,0 +1,25 @@
+
+// npm -g i documentation
+// $ documentation build --config documentation.yml AdminService.js -f html -o api
+// Note: don't upload CSS, else fix in S3
+// Version should sync w/ mbake version 
+
+/**
+ * Version v4.11.23
+ */
+console.log('EditorService', 'v4.11.23')
+
+/**
+* @example
+
+*/
+class AdminService  {
+
+	constructor() {
+
+	}//cons
+
+  
+	
+}//()
+
---
+++ autoEditorsEG/www/EditorService.js
@@ -0,0 +1,27 @@
+
+// npm -g i documentation
+// $ documentation build --config documentation.yml EditorService.js -f html -o api
+// Note: don't upload CSS, else fix in S3
+// Version should sync w/ mbake version 
+
+/**
+ * Version v4.11.23
+ */
+console.log('EditorService', 'v4.11.23')
+
+/**
+* @example
+
+*/
+class EditorService  {
+
+	constructor() {
+
+	}//cons
+
+   getPagesList() { // FIRST
+      
+   }
+	
+}//()
+
