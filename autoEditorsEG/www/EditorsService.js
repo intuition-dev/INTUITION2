ae5b025012b0e22fd88b3673286dd8de86ad59cc
Vic <vic@eml.cc>
2018-11-05 18:43:47 -0500

    k

--- autoEditorsEG/www/EditorsService.js
+++ autoEditorsEG/www/EditorsService.js
@@ -24,6 +24,23 @@ class EditorsService  {
    
 }//()
 
+var config = {
+   apiKey: "AIzaSyBe-aYyf7mnGnpQ0MGX0V8PignXsd-aeHA",
+   authDomain: "auth-f959b.firebaseapp.com",
+   databaseURL: "https://auth-f959b.firebaseio.com",
+   projectId: "auth-f959b",
+   storageBucket: "auth-f959b.appspot.com",
+   messagingSenderId: "421590478204"
+ };
+ firebase.initializeApp(config)
+
 // firebase login
+firebase.auth().signInWithEmailAndPassword('user2@example.com', 'secretPassword').catch(function(error) {
+   // Handle Errors here.
+   var errorCode = error.code;
+   var errorMessage = error.message;
+   console.log()
+   // ...
+ })
 
 // axios
