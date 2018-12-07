f0e714896825b2a0590568038746a34dd5a148f4
Liza K <liza.kislyakova@gmail.com>
2018-12-07 18:56:36 +0300

    edit user add

--- autozAdminEG/www/assets/js/general.js
+++ autozAdminEG/www/assets/js/general.js
@@ -20,6 +20,7 @@ class Editors {
                     ],
                     rowClick:function(e, row){ // fill the form fields
                         var row = row.getData();
+                        window.rowUid = row.id;
                         $('input[name="name"]').val(row.name);
                         $('input[name="email"]').val(row.email);
 
@@ -29,49 +30,6 @@ class Editors {
                     },
                 });
 
-
-                // $('#employees-table').DataTable({
-                //     data: employees.data,
-                //     select: true,
-                //     createdRow: function (row, data, dataIndex) {
-                //         $(row).find('#btn-remove').on('click', function (ev) {
-                //             ev.preventDefault();
-                //             $(this).attr("disabled", "disabled");
-                //             _this
-                //                 .remove(data.id) //update project with active flag
-                //                 .then(() => $(this).removeAttr("disabled"));
-                //         });
-                //         $(row).find('#btn-edit').on('click', function (ev) {
-                //             ev.preventDefault();
-                //             $(this).attr("disabled", "disabled");
-                //             _this
-                //                 .edit(data.id) //update project with active flag
-                //                 .then(() => $(this).removeAttr("disabled"));
-                //         });
-                //     },
-                //     rowId: function (doc) {
-                //         return doc.id;
-                //     },
-                //     columns: [{
-                //         title: 'Email',
-                //         className: 'email',
-                //         data: function (doc) {
-                //             return doc.email || '';
-                //         }
-                //     }, {
-                //         title: 'Name',
-                //         className: 'name',
-                //         data: function (doc) {
-                //             return doc.name || '';
-                //         }
-                //     }, {
-                //         title: '',
-                //         className: 'actions',
-                //         data: function (doc) {
-                //             return '<div class="btn-row end"><a id="edit-employee" class="edit-company-link btn-secondary thin btn" href="/auth/employees/employee-edit/?company=' + companyId + '&button=edit&name=' + doc.name + '&email=' + doc.email + '&employee=' + doc.id + '&companyname=' + companyName+ '">Edit</a><a id="btn-remove" class="remove-company-link btn-secondary thin btn" href="#">Remove</a></div>';
-                //         }
-                //     }]
-                // });
             })
             .then(this.initActionButtons);
     }
@@ -82,18 +40,32 @@ class Editors {
         let email = $("#editor-form input[name='email']").val();
         let name = $("#editor-form input[name='name']").val();
         if (id) { // edit user
-            return this.apiService.editEditor(id)
+            return this.apiService.editEditor(id, name)
                 .then(() => {
                     console.log('user was successfully updated');
-                    alert('user was successfully updated'); // TODO add notification msg instead
-                    // TODO add table refresh
+                    $('.notification').removeClass('d-hide').text('user was successfully updated');
+                    setTimeout(function() {
+                        $('.notification').addClass('d-hide').text('');
+                    }, 10000);
+                    // table refresh
+                    this.table
+                        .updateOrAddData([{id:documentRef.data.id ,email: email, name: name}])
+                        .then(function(){
+                            console.log('table updated');
+                        })
+                        .catch(function(error){
+                            console.log('unable update table', error);
+                        });
                 });
         } else { // add user
             return this.apiService.addEditor(name, email, password)
                 .then((documentRef) => {
                     console.log('new user was created', documentRef.data.id);
-                    alert('new user was created'); // TODO add notification msg instead
-                    // TODO add table refresh
+                    $('.notification').removeClass('d-hide').text('new user was created');
+                    setTimeout(function() {
+                        $('.notification').addClass('d-hide').text('');
+                    }, 10000);
+                    // table refresh
                     this.table
                         .updateOrAddData([{id:documentRef.data.id ,email: email, name: name}])
                         .then(function(){
@@ -108,7 +80,10 @@ class Editors {
                         alert("Unable to create user: " + err.response.data.error);
                     }
                     console.log('err: ', err);
-                    alert('an error occured, user wasn\'t created', err); // TODO add notification msg instead
+                    $('.notification').removeClass('d-hide').addClass('error-msg').text('an error occured, user wasn\'t created', err);
+                    setTimeout(function() {
+                        $('.notification').addClass('d-hide').removeClass('error-msg').text('');
+                    }, 10000);
                 });
         }
     }
