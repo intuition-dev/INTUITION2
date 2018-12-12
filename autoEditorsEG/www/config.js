1b4065f9826679e3d40159f9b469b1a88092d34e
Liza K <liza.kislyakova@gmail.com>
2018-12-12 15:52:22 +0300

    editors config update

--- autoEditorsEG/www/config.js
+++ autoEditorsEG/www/config.js
@@ -3,6 +3,6 @@ config = {
    ,'appPort': 9082
    ,"LOCAL_API": "http://0.0.0.0:9081"
    ,"LOCAL_API_PASS": "123"
-   ,"DEV_API": "http://178.128.163.174:3030"
+   ,"DEV_API": "http://178.62.61.224:9081"
    ,"DEV_API_PASS": "Ce3kvenich"
 }
