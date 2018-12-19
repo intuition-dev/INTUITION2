a8dbd13b3214f944dea4121f7760b08dc3d324a5
Vic <vic@eml.cc>
2018-12-19 10:00:57 -0500

    doc

--- zOldEG0/mnt.sh
+++
@@ -1,3 +0,0 @@
-# ~/goofys --profile default -o allow_other --use-content-type orgblog2 ~/adminEG/prod
-
-~/goofys --profile default -o allow_other --use-content-type BUCKET-NAME ~/adminEG/prod
--- zOldEG0/x.cmd
+++
@@ -1,5 +0,0 @@
-
-call npm i
-call mbake admin_www
-call tsc
-call ts-node index.ts
