2e701f2fd1cad6fc906c80c4d42ed938942e2b09
Vic <vic@eml.cc>
2019-02-09 20:49:26 -0500

    stripe

--- stripe/index.ts
+++ stripe/index.ts
@@ -18,21 +18,29 @@ const PORT = 8444
 exp.use(require('body-parser').urlencoded({extended: false}))
 
 // ////////////////////////////////////////////////
+// https://stripe.com/docs/api/cards/create?lang=curl
+// https://stripe.com/docs/charges
+// https://stripe.com/docs/api/charges/create
+// https://stripe.com/docs/payments/payment-intents/usage
+// https://stackoverflow.com/questions/18374282/stripe-payment-save-token-and-customer-and-make-payment-later-from-token
+// https://stripe.com/docs/saving-cards
+// stripe capture
+// 
 exp.post('/post/charge', (req, res) => {
   let amount = 500;
 
   stripe.customers.create({
-	  email: req.body.stripeEmail,
-	 source: req.body.stripeToken
+     email: req.body.stripeEmail,
+    source: req.body.stripeToken
   })
   .then(customer =>
-	 stripe.charges.create({
-		amount,
-		description: 'Sample Charge',
-			currency: 'usd',
-			customer: customer.id
-	 }))
-  .then(charge => {
+    stripe.charges.create({
+      amount,
+      description: 'Sample Charge',
+         currency: 'usd',
+         customer: customer.id
+    }))
+  .then(charge => { // or just return a message
      console.log(charge)
      res.send('/chargedPg')
   })
