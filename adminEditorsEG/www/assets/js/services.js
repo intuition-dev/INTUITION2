e5221bd2d66aad7fd6001086e132c07b1c20b138
Liza K <liza.kislyakova@gmail.com>
2019-02-14 21:27:08 +0300

    service js documented

--- adminEditorsEG/www/assets/js/services.js
+++ adminEditorsEG/www/assets/js/services.js
@@ -1,91 +1,118 @@
+/**
+ * Version v1.0
+*/
+
+/**
+* CRUD .md files and post creation from blog mounted app
+*/
 class ApiService {
-   constructor(baseURL_) {
-      let token = sessionStorage.getItem('idToken');
-      if (token === null) {
-         auth
-            .signOut()
-            .then(function() {
-               if (window.location.pathname !== '/' && window.location.pathname !== '') {
-                  window.location = ('/');
-               }
-            }).catch(function(error) {
-               alert('An error happened.');
-               console.info('Something went wrong:', error);
-            });
-      }
-      this.service = axios.create({
-         baseURL: baseURL_,
-         headers: {
-            'fb-auth-token': token
-         },
-         responseType: 'json'
-      });
-      //this.isLoading = this.isLoading.bind(this);
 
-      this.service.interceptors.response.use(function(response) {
-         // Do something with response data
-         console.info('response', response);
-         return response;
-      }, function(error) {
-         // With response error redirect
-         if (401 === error.response.status) {
-            // wrong token -- access denied
+    /**
+    * @param baseURL_ base api url (example: http://0.0.0.0:3030/)
+    */
+    constructor(baseURL_) {
+        let token = sessionStorage.getItem('idToken');
+        if (token === null) {
             auth
-               .signOut()
-               .then(function() {
-                  window.location = ('/');
-               }).catch(function(error) {
-                  alert('An error happened.');
-                  console.info('Something went wrong:', error);
-               });
-         }
-         return Promise.reject(error);
-     });
+                .signOut()
+                .then(function() {
+                    if (window.location.pathname !== '/' && window.location.pathname !== '') {
+                        window.location = ('/');
+                    }
+                }).catch(function(error) {
+                    alert('An error happened.');
+                    console.info('Something went wrong:', error);
+                });
+        }
+        this.service = axios.create({
+            baseURL: baseURL_,
+            headers: {
+                'fb-auth-token': token
+            },
+            responseType: 'json'
+        });
+
+        this.service.interceptors.response.use(function(response) {
+            // Do something with response data
+            console.info('response', response);
+            return response;
+        }, function(error) {
+            // With response error redirect
+            if (401 === error.response.status) {
+                // wrong token -- access denied
+                auth
+                .signOut()
+                .then(function() {
+                    window.location = ('/');
+                }).catch(function(error) {
+                    alert('An error happened.');
+                    console.info('Something went wrong:', error);
+                });
+            }
+            return Promise.reject(error);
+        });
    }
 
-   // get list of directories on the left
-   getDirsList() {
-      return this.service.get('/editors/posts');
-   }
+    /**
+    * get list of posts directories
+    */
+    getDirsList() {
+        return this.service.get('/editors/posts');
+    }
 
-   // get sub list of directories on the left
-   getSubDirsList(id) {
-      return this.service.get('/editors/files', {
-         params: {
-            post_id: id
-         }
-      });
-   }
+    /**
+    * get posts directories' subdirectories list
+    * @param id path to post, eg: 'blog/post-2'
+    */
+    getSubDirsList(id) {
+        return this.service.get('/editors/files', {
+            params: {
+                post_id: id
+            }
+        });
+    }
 
-   // get .md file
-   getPostMd(id, pathPrefix) {
-      return this.service.get('/editors/post', {
-         params: {
-            post_id: id,
-            pathPrefix: pathPrefix
-         }
-      });
-   }
+    /**
+    * get .md files
+    * @param id .md file name, eg: '/title.md'
+    * @param pathPrefix path to .md file, eg: 'blog/post-4'
+    */
+    getPostMd(id, pathPrefix) {
+        return this.service.get('/editors/post', {
+            params: {
+                post_id: id,
+                pathPrefix: pathPrefix
+            }
+        });
+    }
 
-   // save .md and mbake after edit
-   savePostMd(id, md, pathPrefix) {
-      return this.service.put('/editors/post', md, {
-         headers: { 'Content-Type': 'text/plain' },
-         params: {
-            post_id: id,
-            pathPrefix: pathPrefix
-         }
-      });
-   }
+    /**
+    * save .md and mbake after edit
+    * @param id .md file name, eg: '/title.md'
+    * @param md .md file content, eg: '###### Lorem ipsum dd dolor sit {.title}'
+    * @param pathPrefix path to .md file, eg: 'blog/post-4'
+    */
+    savePostMd(id, md, pathPrefix) {
+        return this.service.put('/editors/post', md, {
+            headers: { 'Content-Type': 'text/plain' },
+            params: {
+                post_id: id,
+                pathPrefix: pathPrefix
+            }
+        });
+    }
 
-   // create new post
-   createPost(id) {
-      console.info('post_id', id);
-      return this.service.post('/editors/new-post', {}, {
-         params: {
-            post_id: id
-         }
-      });
-   }
+    /**
+    * create new post
+    * @param id new post folder name, eg: 'post-cpv'
+    */
+    createPost(id) {
+        console.info('post_id', id);
+        return this.service.post('/editors/new-post', {}, {
+            params: {
+                post_id: id
+            }
+        });
+    }
 
 }
--- adminzAdminEG/www/assets/js/services.js
+++ adminzAdminEG/www/assets/js/services.js
@@ -1,4 +1,16 @@
+/**
+ * Version v1.0
+*/
+
+/**
+* CRUD users for Editors App
+*/
 class ApiService {
+    /**
+    * basic auth 
+    * @param username user name
+    * @param password user password
+    */
     constructor(username, password) {
         this.service = axios.create({
             baseURL: window.api[0],
@@ -26,12 +38,19 @@ class ApiService {
         });
     }
 
-    // get data for editors table
+    /**
+    * get data for editors table
+    */
     getEditorsList() {
         return this.service.get('/auth/editors');
     }
 
-    // add user to collection through node to FS
+    /**
+    * add new user
+    * @param name user name, eg: 'Jane Doe'
+    * @param email user email, eg: 'example@example.com'
+    * @param password user password, eg: 'dfgsdgdsfg' 
+    */
     addEditor(name, email, password) {
         return this.service.post('/auth/editors', {
             name: name,
@@ -40,7 +59,11 @@ class ApiService {
         });
     }
 
-    // edit user to collection through node to FS
+    /**
+    * edit user name
+    * @param uid user id, eg: 'I3fE7p5NjtV1Y1m5pWBsZlyi4W62'
+    * @param name user name, eg: 'Jane Doe'
+    */
     editEditor(uid, name) {
         return this.service.put('/auth/editors', {
             name: name,
@@ -48,7 +71,10 @@ class ApiService {
         });
     }
 
-    // delete user to collection through node to FS
+    /**
+    * remove user 
+    * @param uid user id, eg: 'I3fE7p5NjtV1Y1m5pWBsZlyi4W62'
+    */
     deleteEditor(uid) {
         return this.service.delete('/auth/editors', {
             params: {
