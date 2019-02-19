68a925819870a43717f5ccf390b9b7fbf144b1b4
Vic <uptimevic@gmail.com>
2019-02-19 15:45:12 -0500

    sync

--- stripe/index.ts
+++ stripe/index.ts
@@ -12,10 +12,9 @@ const stripe = require('stripe')(keys.keySecret); // from keys.yaml
 
 
 // ////////////////////////////////////////////////
-// point the webhook to a public eg: run node on D.O. http://45.55.61.163:8080/webHooks
 const webHookApp = new express();
-webHookApp.use(customCors.cors());
-// Retrieve the raw body as a buffer and match all content types
+//webHookApp.use(customCors.cors());
+
 webHookApp.use(require('body-parser').raw({type: '*/*'}));
 
 const endpointSecret = 'whsec_OTl4uIs3JwBMLXfoH5U1lTU7RAS0kXib'; // should be in yaml
@@ -45,12 +44,9 @@ webHookApp.all('/webHooks', (req, res) => {
 webHookApp.listen(8080);
 
 // ////////////////////////////////////////////////
-// 0 deploy to a DO instance so webhooks work as line 10
-// 1 fix css so radio is aligned
-// 2 use elements for credit card https://stripe.com/docs/stripe-js/elements/quickstart
-// 3 create source f type cc https://stripe.com/docs/sources/cards
-// 4 notice payment methods in stripe docs
-// 5 some sources need to listen to connect to webhook event
+// 1 use elements for credit card https://stripe.com/docs/stripe-js/elements/quickstart
+// 2 create source f type cc https://stripe.com/docs/sources/cards
+// 3 some sources need to listen to connect to webhook event
 
 const PORT = 8444;
 expApp.use(require('body-parser').urlencoded({extended: false}));
@@ -84,7 +80,7 @@ expApp.post('/post/charge', (req, res) => {
     console.info('result ---->', result);
 });
 
-// ////////////////////////////////////////////////
+// static ////////////////////////////////////////////////
 expApp.use(express.static('www'))
 expApp.listen(PORT);
 console.info(PORT);
