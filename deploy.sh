d20e0ea097200472f2be9652a729c23f564f5a11
Liza K <liza.kislyakova@gmail.com>
2019-03-21 21:25:20 +0300

    [CMS]: deploy file update

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 git pull
-rsync --progress -r -u adminzAdminEG/. ~/cms/adminzAdminEG/
 rsync --progress -r -u CMS/. ~/cms/CMS/
 rsync --progress -u README.md ~/cms/README.md
 rsync --progress -u LICENSE ~/cms/LICENSE
