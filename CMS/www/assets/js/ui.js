21ec85c38d454e81bc1e805e149c00814c84c572
Liza K <liza.kislyakova@gmail.com>
2019-04-04 19:31:10 +0300

    [cms]: forms and icons active state add, styles update

--- CMS/www/assets/js/ui.js
+++ CMS/www/assets/js/ui.js
@@ -1,30 +1,32 @@
 depp.require('deviceready', function() {
 
-   // login form input lables animation
-   if ($('input[name="login"]:-webkit-autofill').length > 0) {
-      $('input[name="login"]').parents('.input-wrap').find('label').addClass('focus');
-   }
-   if ($('input[name="password"]:-webkit-autofill').length > 0) {
-      $('input[name="password"]').parents('.input-wrap').find('label').addClass('focus');
-   }
+    // login form input lables animation
+    if ($('input[name="login"]:-webkit-autofill').length > 0) {
+        $('input[name="login"]').parents('.input-wrap').find('label').addClass('focus');
+    }
+    if ($('input[name="password"]:-webkit-autofill').length > 0) {
+        $('input[name="password"]').parents('.input-wrap').find('label').addClass('focus');
+    }
 
-   $('.login-form input').focus(function() {
-      var label = $(this).parents('.input-wrap').find('label');
-      if ($(this).val() === '') {
-         label.addClass('focus');
-      }
-   });
+    $('.login-form input').focus(function() {
+        var label = $(this).parents('.input-wrap').find('label');
+        if ($(this).val() === '') {
+            label.addClass('focus');
+        }
+    });
 
-   $('.login-form input').focusout(function() {
-      var label = $(this).parents('.input-wrap').find('label');
-      if ($(this).val() === '') {
-         label.removeClass('focus');
-      }
-   });
+    $('.login-form input').focusout(function() {
+        var label = $(this).parents('.input-wrap').find('label');
+        if ($(this).val() === '') {
+            label.removeClass('focus');
+        }
+    });
 
 
-   $('.user-name').text(sessionStorage.getItem('user_name'));
+    $('.user-name').text(sessionStorage.getItem('user_name'));
 
-   $('.datepicker').Zebra_DatePicker();
+    $('.datepicker').Zebra_DatePicker();
+
+    $('.site-brand').text(siteName);
 
 });
