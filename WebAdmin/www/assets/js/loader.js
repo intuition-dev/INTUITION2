2dd98c7943074fef57dbda57b4e0634dd97b76b0
Liza K <liza.kislyakova@gmail.com>
2019-04-12 20:47:33 +0300

    [WebAdmin]: code fix, gitignore update

---
+++ WebAdmin/www/assets/js/loader.js
@@ -0,0 +1,51 @@
+document.addEventListener("DOMContentLoaded", onDeviceReady, false);
+
+function onDeviceReady() {
+    console.info('deviceready');
+    depp.done('deviceready');
+}
+
+var responces = [];
+
+depp.define({
+    'scripts': [
+        // ,'//cdn.jsdelivr.net/npm/tabulator-tables@4.2.3/dist/js/tabulator.min.js'
+        '//cdn.jsdelivr.net/npm/jquery@3.3.1/dist/jquery.slim.min.js'
+        , '/assets/3rd/gridforms.js'
+        , '//unpkg.com/axios@0.19.0-beta.1/dist/axios.min.js'
+        , '//cdn.jsdelivr.net/npm/zebra_datepicker@1.9.6/dist/zebra_datepicker.min.js'
+        , ROOT + 'assets/css/spectreBottom.css'
+    ]
+});
+
+depp.require(['scripts'], function() {
+    depp.define({
+        'services': [
+            '/assets/js/db.js'
+            , '/assets/js/services.js'
+        ]
+    });
+});
+
+depp.require(['services'], function() {
+    depp.define({
+        'general': [
+            '/assets/js/general.js'
+            ,'/assets/js/login.js'
+        ]
+    });
+});
+
+depp.require(['general'], function() {
+    depp.define({
+        'rw': [
+            '/assets/js/ui.js'
+        ],
+        'fonts': [
+            '#rw'
+            ,'css!//fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i'
+        ]
+    });
+});
+
+depp.require(['fonts']);
--- WebAdmin/wwwAdmin/assets/js/loader.js
+++ WebAdmin/wwwAdmin/assets/js/loader.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", onDeviceReady, false)
 
 function onDeviceReady() {
-    console.info('deviceready')
-    depp.done('deviceready')
+    console.info('deviceready');
+    depp.done('deviceready');
 }
 
 var responces = [];
@@ -11,10 +11,9 @@ depp.define({
   'scripts': [
     '//cdn.jsdelivr.net/npm/jquery@3.3.1/dist/jquery.min.js'
     ,'//cdn.jsdelivr.net/npm/tabulator-tables@4.2.3/dist/js/tabulator.min.js'
-    ,'//cdn.jsdelivr.net/npm/tabulator-tables@4.2.3/dist/css/tabulator.min.css'
-    , '//cdn.jsdelivr.net/npm/tabulator-tables@4.2.3/dist/css/tabulator_simple.min.css'
     , '/assets/3rd/gridforms.js'
     , '//unpkg.com/axios@0.19.0-beta.1/dist/axios.min.js'
+    , ROOT + 'assets/css/spectreBottom.css'
   ]
 });
 
@@ -39,8 +38,12 @@ depp.require(['general'], function() {
     'rw': [
       '/assets/js/login.js'
       , '/assets/js/ui.js'
+    ],
+    'fonts': [
+      '#rw'
+      ,'css!//fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i'
     ]
   });
 });
 
-depp.require(['rw']);
+depp.require(['fonts']);
