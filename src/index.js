dfe6722636439fc8e8217b0c116e0fc386b2b8df
Natalia Melikyants <melikyants@gmail.com>
2019-06-04 14:18:48 +0200

    rm setup.ts

---
+++ src/index.js
@@ -0,0 +1,67 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Serv_1 = require("mbake/lib/Serv");
+const editor_1 = require("./lib/editor");
+const admin_1 = require("./lib/admin");
+const ADB_1 = require("./lib/ADB");
+const bodyParser = require("body-parser");
+const mainAppG = Serv_1.ExpressRPC.makeInstance(['http://localhost:9080']);
+const appGPORT = '9081';
+const fs = require('fs');
+const pathToDb = 'ADB.sqlite';
+const config_port = 3100;
+mainAppG.use(bodyParser.json());
+mainAppG.use(bodyParser.text());
+mainAppG.use(bodyParser.urlencoded({ extended: true }));
+try {
+    if (fs.existsSync(pathToDb)) {
+        const editorRoutes = new editor_1.EditorRoutes();
+        mainAppG.use('/api/editors', editorRoutes.routes());
+        mainAppG.use('/editors', Serv_1.ExpressRPC.serveStatic('www'));
+        const adminRoutes = new admin_1.AdminRoutes();
+        mainAppG.use('/api/admin', adminRoutes.routes());
+        mainAppG.use('/admin', Serv_1.ExpressRPC.serveStatic('wwwAdmin'));
+    }
+    else {
+        fs.open('ADB.sqlite', 'w', runSetup);
+    }
+}
+catch (err) {
+}
+function runSetup() {
+    mainAppG.use('/setup', Serv_1.ExpressRPC.serveStatic('setup'));
+}
+mainAppG.post("/setup", async (req, res) => {
+    const method = req.fields.method;
+    let params = JSON.parse(req.fields.params);
+    let email = params.email;
+    let password = params.password;
+    var salt = bcrypt.genSaltSync(10);
+    var hashPass = bcrypt.hashSync(password, salt);
+    let emailjs = params.emailjs;
+    let pathToSite = params.pathToSite;
+    let resp = {};
+    if ('setup' == method) {
+        resp.result = {};
+        try {
+            await ADB_1.ADB.createNewADBwSchema();
+            await db.run(`CREATE TABLE admin(email,password,emailJsCode, pathToSite)`);
+            await db.run(`INSERT INTO admin(email, password, emailJsCode, pathToSite) VALUES('${email}', '${hashPass}', '${emailjs}', '${pathToSite}')`, function (err) {
+                if (err) {
+                }
+            });
+        }
+        catch (err) {
+        }
+    }
+    else {
+        return res.json(resp);
+    }
+});
+mainAppG.listen(appGPORT, () => {
+    console.log(`mainAppG listening on port ${appGPORT}!`);
+    console.log(`======================================================`);
+    console.log(`App is running at http://localhost:${appGPORT}/editors/`);
+    console.log(`Admin is running at http://localhost:${appGPORT}/admin/`);
+    console.log(`======================================================`);
+});
--- src/index.ts
+++ src/index.ts
@@ -2,28 +2,96 @@ import { ExpressRPC } from 'mbake/lib/Serv';
 import { EditorRoutes } from './lib/editor';
 import { Wa } from 'mbake/lib/Wa';
 import { AdminRoutes } from './lib/admin';
+import { ADB } from './lib/ADB';
 
+const bodyParser = require("body-parser");
 const mainAppG = ExpressRPC.makeInstance(['http://localhost:9080']);
 const appGPORT = '9081';
 
-/*
-* E D I T O R S
-*/
+const fs = require('fs')
+const pathToDb = 'ADB.sqlite'
+const config_port = 3100
 
-const editorRoutes = new EditorRoutes();
-mainAppG.use('/api/editors', editorRoutes.routes());
-mainAppG.use('/editors', ExpressRPC.serveStatic('www'));
+mainAppG.use(bodyParser.json());
+mainAppG.use(bodyParser.text());
+mainAppG.use(bodyParser.urlencoded({ extended: true })); //To handle HTTP POST request in Express
 
 
-// Wa.watch('/Users/liza/work/mbakeCLI/CMS', 9082);
+try {
+   if (fs.existsSync(pathToDb)) {
+      //file exists
+      // mainAppG.use(ExpressRPC.serveStatic('.'));
+      /*
+      * E D I T O R S
+      */
 
-/*
-* A D M I N
-*/
+      const editorRoutes = new EditorRoutes();
+      mainAppG.use('/api/editors', editorRoutes.routes());
+      mainAppG.use('/editors', ExpressRPC.serveStatic('www'));
 
-const adminRoutes = new AdminRoutes();
-mainAppG.use('/api/admin', adminRoutes.routes());
-mainAppG.use('/admin', ExpressRPC.serveStatic('wwwAdmin'));
+
+      // Wa.watch('/Users/liza/work/mbakeCLI/CMS', 9082);
+
+      /*
+      * A D M I N
+      */
+
+      const adminRoutes = new AdminRoutes();
+      mainAppG.use('/api/admin', adminRoutes.routes());
+      mainAppG.use('/admin', ExpressRPC.serveStatic('wwwAdmin'));
+
+      //open admin and editor
+   } else {
+      fs.open('ADB.sqlite', 'w', runSetup);
+      // fs.writeFile('ADB.sqlite', '', runSetup)
+   }
+
+} catch (err) {
+}
+
+function runSetup() {
+   mainAppG.use('/setup', ExpressRPC.serveStatic('setup'));
+   // mainAppG.use(ExpressRPC.serveStatic('setup'));
+}
+
+mainAppG.post("/setup", async (req, res) => {
+   const method = req.fields.method;
+   let params = JSON.parse(req.fields.params)
+
+   let email = params.email
+
+   let password = params.password
+   var salt = bcrypt.genSaltSync(10);
+   var hashPass = bcrypt.hashSync(password, salt);
+
+   // guid for pk client side 
+   // eg: bcrypt randomBytes(16).toString("hex") or base64, or Math.random to make base64 char 16 times
+   // also to email a random # 
+   let emailjs = params.emailjs
+   let pathToSite = params.pathToSite
+
+   let resp: any = {}; // new response that will be set via the specific method passed
+   if ('setup' == method) {
+      resp.result = {}
+      // res.send(resp)
+
+      try {
+         await ADB.createNewADBwSchema()
+
+         await db.run(`CREATE TABLE admin(email,password,emailJsCode, pathToSite)`);
+         await db.run(`INSERT INTO admin(email, password, emailJsCode, pathToSite) VALUES('${email}', '${hashPass}', '${emailjs}', '${pathToSite}')`, function (err) {
+            if (err) {
+            }
+            // get the last insert id
+         });
+      } catch (err) {
+         // next(err);
+      }
+   } else {
+      return res.json(resp);
+   }
+});
+
 
 mainAppG.listen(appGPORT, () => {
    console.log(`mainAppG listening on port ${appGPORT}!`);
