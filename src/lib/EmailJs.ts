d4add17f27dea4eeaa9a0020c0c6a11edd20f219
Liza K <liza.kislyakova@gmail.com>
2019-06-04 20:00:00 +0300

    [WebAdmin]: emailJs settings example add

--- src/lib/EmailJs.ts
+++ src/lib/EmailJs.ts
@@ -4,14 +4,14 @@ import axios from 'axios';
 export class EmailJs {
     send(email, from_name, emailjsService_id, emailjsTemplate_id, emailjsUser_id) {
         axios.post('https://api.emailjs.com/api/v1.0/email/send', {
-                service_id: emailjsService_id,
-                template_id: emailjsTemplate_id,
-                user_id: emailjsUser_id,
+                service_id: emailjsService_id, // gmail
+                template_id: emailjsTemplate_id, // template_IWM2Sp3V
+                user_id: emailjsUser_id, // user_PFE8rcsDafCMuUvI1Yj11
                 template_params: {
                     email: email,
                     to_name: email,
                     from_name: from_name,
-                    message_html: 'Hi, your email and password is registered as login credentials for WebAdmin!',
+                    message_html: 'Hi, your email and password are registered as login credentials for WebAdmin!',
                     email_to: email
                 }
             })
