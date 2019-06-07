74b62ec8a22a1b0ff823af5e4baf3e61bf285dd2
Vic <uptimevic@gmail.com>
2019-06-07 17:52:59 -0400

    swap

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-# WebAdmin
-
-Can be installed via yarn install, go to WebAdmin to see that.
-
-Has an API, docs in api.
+src/README.md
--- src/x.sh
+++ src/x.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+clear
 tsc
-node index*.js
+
+node mbake.js -c . 
+
+# node mbakeX.js -c /Users/code/Documents/GitHub/mbakeCake/metaCake/surveryitem/comps
+
+# node mbakeX.js -w /Users/code/Documents/GitHub/metaCake/metaCake/webC
