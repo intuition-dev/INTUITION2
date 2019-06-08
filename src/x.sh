de81da2f5397e32ff3fe461ba4d8122b14f2550e
Vic <uptimevic@gmail.com>
2019-06-08 08:48:52 -0400

    -x

--- src/x.sh
+++ src/x.sh
@@ -4,6 +4,6 @@ tsc
 
 node mbake.js -c . 
 
-# node mbakeX.js -c /Users/code/Documents/GitHub/mbakeCake/metaCake/surveryitem/comps
+# node mbake-x.js -c /Users/code/Documents/GitHub/mbakeCake/metaCake/surveryitem/comps
 
-# node mbakeX.js -w /Users/code/Documents/GitHub/metaCake/metaCake/webC
+# node mbake-x.js -w /Users/code/Documents/GitHub/metaCake/metaCake/webC
