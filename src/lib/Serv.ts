b36bc9707eaef76020a0eaddf8388fe2cee7fd47
Vic <uptimevic@gmail.com>
2019-06-28 13:12:38 -0400

    touched

--- src/lib/Serv.ts
+++ src/lib/Serv.ts
@@ -97,7 +97,7 @@ export class ExpressRPC {
       res.json(resp)
    }
     */
-   handleRRoute2(route:string, pgOrScreen:string, foo:Function) {
+   handleRRoute(route:string, pgOrScreen:string, foo:Function) {
       const r: string = '/'+route  + '/'+pgOrScreen
       this.appInst.post(r, foo)
    }
@@ -107,7 +107,7 @@ export class ExpressRPC {
     * @param route RPC route
     * @param foo function
     */
-   handleRRoute(route:string, foo:Function) {
+   handleRRoute0(route:string, foo:Function) {
       this.appInst.post(route, foo)
    }
 
@@ -131,32 +131,35 @@ export class ExpressRPC {
 }//class
 
 export class RPCBasicAuth {
+
    auth(user, password) {
 
-       // base64 encode
-       let buffUser = new Buffer(user);  
-       user = buffUser.toString('base64');
-       let buffPwd = new Buffer(password);  
-       password = buffPwd.toString('base64');
+      // base64 encode
+      let buffUser = new Buffer(user)
+      user = buffUser.toString('base64')
+      let buffPwd = new Buffer(password)
+      password = buffPwd.toString('base64')
 
-       return (request, response, next) => {
-           if (typeof request.fields.user === 'undefined'
-               || typeof request.fields.pswd === 'undefined'
-           ) {
-               console.info('user or pswd not exist');
-               response.status(401).send();
-           } else if (request.fields.user !== user
-               || request.fields.pswd !== password
-           ) {
-               console.info('user or pswd are not correct');
-               response.status(401).send();
-           } else {
-               console.info('basic auth: success');
-               return next();
-           }
-       }
-   };
-}
+      return (request, response, next) => {
+         if (typeof request.fields.user === 'undefined'
+            || typeof request.fields.pswd === 'undefined'
+         ) {
+            console.info('user or pswd not exist');
+            response.status(401).send();
+         } else if (request.fields.user !== user
+            || request.fields.pswd !== password
+         ) {
+            console.info('user or pswd are not correct');
+            response.status(401).send();
+         } else {
+            console.info('basic auth: success');
+            return next();
+         }
+      }
+
+   }//()
+}//
+
 
 module.exports = {
    ExpressRPC, RPCBasicAuth
