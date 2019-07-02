648d2326cb4f88a1bff0e9d72fcd968caf9df95e
Vic <uptimevic@gmail.com>
2019-07-02 18:59:50 -0400

    ok

--- toolBelt/bootStrap/loader.js
+++ toolBelt/bootStrap/loader.js
@@ -1,7 +1,2 @@
 
-// depp.define({'style':'css/bootstrap.css'})
-
-depp.require(['FontsLoaded','bsDefaultStyle'], function() {
-   console.log('ready')
-
-}) 
+depp.require(['FontsLoaded','bsDefaultStyle'])
