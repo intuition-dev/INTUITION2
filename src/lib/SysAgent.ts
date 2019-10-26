3670705c7331b6fbcbca2cd5a2860231de1026a2
vic <uptimevic@gmail.com>
2019-10-26 17:50:06 -0400

    ok

--- src/lib/SysAgent.ts
+++ src/lib/SysAgent.ts
@@ -2,7 +2,7 @@
 
 var logger = require('tracer').console()
 
-export class SysAgent { // agent
+export class SysAgent { 
     static guid = require('uuid/v4')
 
     static si = require('systeminformation')
@@ -66,50 +66,5 @@ export class SysAgent { // agent
         })
     }//()
 
-    static async _info() { // rare, like day
-        /*
-        logger.trace('info')
-
-        await SysAgent.si.services('node, pm2, caddy').then(data =>  {
-            for(let o of data) 
-                delete o['startmode']
-            
-            //track['services']=data
-        })
-
-        SysAgent.si.networkConnections().then(data => logger.trace(data))
-
-        SysAgent.si.processes().then(data => logger.trace(data))
-
-        SysAgent.si.networkInterfaces().then(data => logger.trace(data))
-
-        SysAgent.si.fsSize().then(data => logger.trace(data))
-
-        SysAgent.si.blockDevices().then(data => logger.trace(data))
-
-        SysAgent.si.osInfo().then(data => logger.trace(data))
-
-        SysAgent.si.users().then(data => logger.trace(data))
-
-        */
-    }//()
-
 }//class
 
-// network. geocode. ip. volume free, ports, who
-
-// fail over
-
-// https://www.npmjs.com/package/portscanner
-
-// https://github.com/ddsol/speedtest.net#readme
-
-// https://www.npmjs.com/package/pcap
-
-//https://www.npmjs.com/package/ip
-
-// https://www.npmjs.com/package/local-devices
-
-// https://millermedeiros.github.io/mdoc/examples/node_api/doc/os.html
-
-// has
